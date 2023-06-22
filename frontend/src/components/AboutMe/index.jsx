8f992365e13414983029d4be392e91c88efe8e3a
Pythonic <gabrieloliveirafsociety@gmail.com>
2023-06-22 16:00:38 -0300

    feat: completed about page responsive

--- frontend/src/components/AboutMe/index.jsx
+++ frontend/src/components/AboutMe/index.jsx
@@ -6,14 +6,14 @@ export default function AboutMe() {
 			<div className={styles.aboutImg}>
 				<div className={styles.aboutImgBox}>
 					<div className={styles.welcome}>
-						<p>Olá, seja bem-vindo ao meu<br/>
-							<span className={styles.colorNeon}>portfolio de fotos.</span>
+						<p>Olá, seja bem-vindo ao meu
+							<span> portfolio de fotos.</span>
 						</p>
 					</div>
 					<div className={styles.description}>
 						<p>
 							Me chamo Vitor e trabalho como fotógrafo há 15 anos, e aqui você encontrará uma coletânea dos meus melhores trabalhos em todos esses anos.<br/>
-							<span className={styles.colorNeon}>Divirta-se.</span>
+							<span>Divirta-se.</span>
 						</p>
 					</div>
 				</div>
